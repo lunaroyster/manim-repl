b7d7bd361b05f8b6a1678d2d1fcc9c37c38927d6
lunaroyster <lunaroyster@users.noreply.github.com>
2019-12-13 18:55:20 -0500

    Fixed sed command

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.x
 mkdir $PWD/ffmpeg
 tar xf ffmpeg.tar.xz -C $PWD/ffmpeg --strip-components=1
 chmod 777 $PWD/ffmpeg/ffmpeg
-sed -i 's/^FFMPEG_BIN = "ffmpeg"$/# FFMPEG_BIN = "ffmpeg"\nFFMPEG_BIN = "$PWD\ffmpeg\/ffmpeg"/g' $PWD/manim/manimlib/constants.py
+sed -i "s/^FFMPEG_BIN = \"ffmpeg\"$/# FFMPEG_BIN = \"ffmpeg\"\nFFMPEG_BIN = \"$PWD\/ffmpeg\/ffmpeg\"/g" $PWD/manim/manimlib/constants.py
 python -m pip install -r $PWD/manim/requirements.txt
