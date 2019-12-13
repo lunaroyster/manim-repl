3957bc1bea5c2c5bf020e31a3ecd496d5841bbc7
lunaroyster <lunaroyster@users.noreply.github.com>
2019-12-13 17:49:43 -0500

    Made installation directory explicit

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
-cd /home/runner/manim-repl/
+echo "Installing in $PWD"
 git clone https://github.com/3b1b/manim
-wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -O sneakyffmpeg.tar.xz
-mkdir sneakyffmpeg
-tar xf sneakyffmpeg.tar.xz -C sneakyffmpeg --strip-components=1
-chmod 777 ./sneakyffmpeg/ffmpeg
-cd /home/runner/manim-repl/manim
-sed -i 's/^FFMPEG_BIN = "ffmpeg"$/# FFMPEG_BIN = "ffmpeg"\nFFMPEG_BIN = "\/home\/runner\/manim-repl\sneakyffmpeg\/ffmpeg"/g' ./manimlib/constants.py
-python -m pip install -r requirements.txt
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -O $PWD/ffmpeg.tar.xz
+mkdir $PWD/ffmpeg
+tar xf ffmpeg.tar.xz -C $PWD/ffmpeg --strip-components=1
+chmod 777 $PWD/ffmpeg/ffmpeg
+sed -i 's/^FFMPEG_BIN = "ffmpeg"$/# FFMPEG_BIN = "ffmpeg"\nFFMPEG_BIN = "$PWD\ffmpeg\/ffmpeg"/g' $PWD/manim/manimlib/constants.py
+python -m pip install -r $PWD/manim/requirements.txt
